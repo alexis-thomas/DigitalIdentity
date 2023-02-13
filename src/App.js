721c70216e6e67f24b04e9964fb1f60bfb213622
alexis <alexis.thomasjutisz@gmail.com>
2023-02-13 18:39:04 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MantineProvider, useMantineTheme } from "@mantine/core";
+import { useState } from "react";
+import { Drawer, Button, Group, Burger, NavLink } from "@mantine/core";
 
 function App() {
+  const [opened, setOpened] = useState(true);
+  const theme = useMantineTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+        colors: {
+          // override dark colors to change them for all components
+          dark: [
+            "#d5d7e0",
+            "#acaebf",
+            "#8c8fa3",
+            "#666980",
+            "#4d4f66",
+            "#34354a",
+            "#2b2c3d",
+            "#1d1e30",
+            "#0c0d21",
+            "#01010a",
+          ],
+        },
+      }}
+    >
+      <div className="App">
+        <header className="App-header">
+          <div className="Banner">
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((o) => !o)}
+              title="test"
+            />
+            <div>Bio</div>
+            <div>Resume</div>
+            <div>Research & teaching</div>
+            <div>Awards</div>
+          </div>
+          <Button>Click me!</Button>
+          <Drawer
+            opened={opened}
+            overlayColor={
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[9]
+                : theme.colors.gray[2]
+            }
+            onClose={() => setOpened(false)}
+            title="Register"
+            size="xl"
+            closeOnClickOutside={false}
+            className="Drawer"
+            trapFocus={false}
+            withOverlay={false}
+          >
+            <NavLink label="With icon" />
+          </Drawer>
+          <div>Bonjour je suis Nathan un jeune étudiant</div>
+        </header>
+      </div>
+    </MantineProvider>
   );
 }
 
