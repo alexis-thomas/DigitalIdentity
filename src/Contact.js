e0c5ea71ce183a87b1ce377c1fc2d9784391b606
Alexis Thomas <talexist@amazon.com>
2023-02-24 18:24:27 +0100

    Contact page first version

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,24 @@
+import "./Contact.css"
+import {
+  SiMaildotru,
+  SiLinkedin} from "react-icons/si";
+import { Card, Image, Text, Badge, Button, Group, Title, Grid } from "@mantine/core";
+
+
+// Function that displays all my contact details and social media accounts
 function Contact() {
-  return <div className="contact">nathan@doumdoum.fr</div>;
+  return (
+    <div className="contact">
+      <Title order={2}>Contact</Title>
+
+      <div class="details">
+<SiMaildotru className="details-icon" size={20}/>
+      <a href="mailto: nathan@doumdoum.fr" className="details-value">nathan@doumdoum.fr</a>
+      <SiLinkedin className="details-icon"  size={20}/>
+      <a href="https://linkedin.fr/nathou" className="details-value">https://linkedin.fr/nathou</a>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
