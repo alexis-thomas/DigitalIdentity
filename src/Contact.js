2ace7ca35443df55b812968d4859c28ba8e0b112
Nathan Doumeche <nathan.doumeche@gmail.com>
2023-02-26 17:47:10 +0100

    setup gh-pages

--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,19 @@
-import "./Contact.css"
-import {
-  SiMaildotru,
-  SiLinkedin} from "react-icons/si";
-import { Card, Image, Text, Badge, Button, Group, Title, Grid } from "@mantine/core";
-
+import "./Contact.css";
+import { SiMaildotru, SiLinkedin } from "react-icons/si";
+import { Title } from "@mantine/core";
 
 // Function that displays all my contact details and social media accounts
 function Contact() {
   return (
     <div className="contact">
-      <Title order={2}>Contact</Title>
+      <Title order={1}>Contact</Title>
 
       <div class="details">
-<SiMaildotru className="details-icon" size={20}/>
-      <a href="mailto: nathan@doumdoum.fr" className="details-value">nathan@doumdoum.fr</a>
-      <SiLinkedin className="details-icon"  size={20}/>
-      <a href="https://linkedin.fr/nathou" className="details-value">https://linkedin.fr/nathou</a>
+        Please contact me at{" "}
+        <a href="mailto: nathan.doumeche@gmail.com" className="details-value">
+          nathan.doumeche@gmail.com
+        </a>
+        .
       </div>
     </div>
   );
